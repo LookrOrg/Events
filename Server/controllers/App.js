a380922b931ebcfe14b970132f495e1b9ac64332
Luca-Lucidera <82151241+Luca-Lucidera@users.noreply.github.com>
2022-06-06 08:52:14 +0200

    Added Signup

--- Server/controllers/App.js
+++ Server/controllers/App.js
@@ -32,16 +32,53 @@ const login = async (req, res) => {
 const signup = async (req, res) => {
   const user = req.body;
   try {
+    //controllo la password
     if (validate(user.email)) {
+      //prendo un utente che ha la stessa mail
       const checkUser = await prisma.users.findFirst({
         where: {
           email: user.email,
         },
       });
-      if(!checkUser){
-        
-      }else{
-        res.status(400).json({error: "user already exist"})
+      //se non esiste continuo
+      if (!checkUser) {
+        //controllo che le password siano uguali
+        if (user.password == user.confirmPassword) {
+          const regex = new RegExp(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+          );
+          //controllo la validità della password (otto caratteri, 1 upper case, 1 lower case, 1 special character
+          if (regex.exec(user.password)) {
+            //cripto la password
+            const hash = bcrypt.hashSync(user.password, 10);
+            user.password = hash;
+            const { confirmPassword, ...User } = user;
+            //creo l'utente nel db
+            try {
+              const newUser = await prisma.users.create({
+                data: User,
+              });
+              if (newUser) {
+                res.status(200).json({ ok: "user created" });
+              } else {
+                res
+                  .status(500)
+                  .json({ error: "user not created, error in the server" });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).json({ error: "Internal server error" });
+            }
+          } else {
+            res.status(400).json({
+              error: "the password doesn't match the required parameter",
+            });
+          }
+        } else {
+          res.status(400).json({ error: "password are different" });
+        }
+      } else {
+        res.status(400).json({ error: "user already exist" });
       }
     } else {
       res.status(400).json({ error: "email is invalid" });
@@ -71,6 +108,7 @@ const checkEmail = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
 
 module.exports = {
   login,
