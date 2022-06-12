349ce2f03674e4d8ad0d6f676c1414b82922c860
JackMarelli <marelligiacomo@gmail.com>
2022-06-12 18:39:18 +0200

    enabled css vars (thx eternal)

--- Client/components/LoginForm.js
+++ Client/components/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { flushSync } from "react-dom";
 import { View, Text, Button, TextInput, Image, StyleSheet } from "react-native";
+import styles from "../App.module.css";
 
 export default function LoginForm() {
   const [state, setState] = useState({
@@ -14,22 +16,40 @@ export default function LoginForm() {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
+        borderWidth: 1,
+        borderColor: "red",
       }}
     >
       <Image alt="Lookr Text Logo" />
-      <View>
+      <View
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderWidth: 1,
+          borderColor: "lime",
+        }}
+      >
         <Text>Log in</Text>
         <TextInput
+          style={styles.input}
           placeholder="example@gmail.com"
         ></TextInput>
+      </View>
+      <View>
+        <Text
+          style={{
+            fontSize: 44,
+          }}
+        >
+          or
+        </Text>
+        <View>
+          <Text>Register</Text>
+          <Text>a new account</Text>
+        </View>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  input: {
-    witdh: "80%",
-    backgroundColor: "gray"
-  }
-})
