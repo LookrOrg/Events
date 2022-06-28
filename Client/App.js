3048c69eda39a7ce5c39213db9ad022ae5f25497
SamueleMornatta <samuelemornatta@gmail.com>
2022-06-28 21:34:34 +0200

    remove react redux and added react query

--- Client/App.js
+++ Client/App.js
@@ -2,8 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Test from "./components/test.js";
 import LoginForm from "./components/LoginForm.js";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
 import * as SecureStore from "expo-secure-store";
 
 SecureStore.getItemAsync("lookertoken").then((token) => {
@@ -14,18 +12,16 @@ SecureStore.getItemAsync("lookertoken").then((token) => {
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <LoginForm />
-      </View>
-    </Provider>
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <LoginForm />
+    </View>
   );
 }
